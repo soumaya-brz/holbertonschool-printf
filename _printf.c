734eba8d57cfe4cb184c63936e2f46e6ac3c32b4
soumaya <soumayabraziii@gmail.com>
2025-11-24 22:35:53 +0000

    fix: correction betty

--- _printf.c
+++ _printf.c
@@ -1,10 +1,35 @@
 #include "main.h"
 
+/**
+ * _printf - Produces output according to a format
+ * @format: A string containing zero or more format specifiers
+ *
+ * Description:
+ * This function is a simplified recreation of the standard printf.
+ * It scans the format string character by character:
+ *
+ * - If the character is not '%', it is printed directly.
+ *   - If a '%' is encountered, the function checks the next character
+ *   to determine which conversion specifier to use.
+ *
+ *   The function delegates the handling of each specifier to
+ *   dedicated helper functions (such as print_char, print_string,
+ *   print_percent, etc.).
+ *
+ *   Supported specifiers in this version:
+ *   %c → print a character
+ *   %s → print a string
+ *    %% → print a literal '%'
+ *
+ *    Return:
+ *    The total number of characters printed. If `format` is NULL,
+ *    the function returns -1.
+ */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int i = 0, printed = 0;
-	
+
 	if (!format)
 		return (-1);
 
@@ -19,7 +44,7 @@ int _printf(const char *format, ...)
 			else if (format[i] == 's')
 				printed += print_string(args);
 			else if (format[i] == '%')
-			       	printed += print_percent();
+				printed += print_percent();
 			else
 			{
 				_putchar('%');
@@ -35,5 +60,5 @@ int _printf(const char *format, ...)
 		i++;
 	}
 	va_end(args);
-	return printed;
+	return (printed);
 }
--- print_c_s.c
+++ print_c_s.c
@@ -1,12 +1,29 @@
 #include "main.h"
 
+/**
+ * print_char - prints a character
+ * @args: va_list containing the character to print
+ *
+ * Return: Always returns 1 (the number of characters printed)
+ */
 int print_char(va_list args)
 {
 	char c = (char)va_arg(args, int);
+
 	_putchar(c);
-	return 1;
+	return (1);
 }
 
+/**
+ * print_string - Prints a string
+ * @args: va_list containing the string to print
+ *
+ * description:
+ * If the string is NULL, this function prints "(null)" to mimic
+ * the behavior of the standard printf.
+ *
+ * Return: number of character printed
+ */
 int print_string(va_list args)
 {
 	char *s = va_arg(args, char *);
@@ -15,12 +32,13 @@ int print_string(va_list args)
 	if (!s)
 	{
 		char *nulls = "(null)";
+
 		while (nulls[i])
 		{
 			_putchar(nulls[i]);
 			i++;
 		}
-		return i;
+		return (i);
 	}
 
 	while (s[i])
@@ -28,11 +46,16 @@ int print_string(va_list args)
 		_putchar(s[i]);
 		i++;
 	}
-	return i;
+	return (i);
 }
 
+/**
+ * print_percent - Prints a percent sign '%'
+ *
+ *Return: Always returns 1 (the number of characters printed)
+ */
 int print_percent(void)
 {
 	_putchar('%');
-	return 1;
+	return (1);
 }
